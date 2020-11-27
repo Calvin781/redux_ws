daa5cdba87c49ab5ed14b93172a6f3408f96a239
Stephane <kalonjistephane@gmail.com>
2020-11-27 14:27:28 +0100

    step1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 
 class Navbar extends Component{
     render(){
-        return <>
-            **NAVBAR COMPONENTS**
-        </>
+        return(
+            <nav className="nav-wrapper">
+                <div className="container">
+                    <Link to="/" className="brand-logo">BeShop</Link>
+                    <ul className="right">
+                        <li><Link to="/">Shop</Link></li>
+                        <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
+                        <li className="qtItems">({this.props.qt})</li>
+                    </ul>
+                </div>
+            </nav>
+        );
     }
 }
 
-export default Navbar;
+const mapStateToProps = (state)=>{
+    return {
+        qt: state.qtItems
+    }
+}
+
+export default  connect(mapStateToProps)(Navbar);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import {createStore} from 'redux'
+import {Provider} from 'react-redux';
+import cartReducer from './components/reducers/cartReducer'
+
+const store = createStore(cartReducer);
+
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
